a341da0f11568243cb70d5199b7442f7d69e63cc
WhiteInfamous24 <juanmanuel.caceres24@outlook.com>
2023-05-08 23:40:03 -0300

    update

--- PIC-LightTracker.X/Main.s
+++ PIC-LightTracker.X/Main.s
@@ -1,4 +1,3 @@
-#include "pic16f887.inc"
 #include "xc.inc"
 
 ; CONFIG1
@@ -17,29 +16,114 @@
   CONFIG  BOR4V = BOR40V        ; Brown-out Reset Selection bit (Brown-out Reset set to 4.0V)
   CONFIG  WRT = OFF             ; Flash Program Memory Self Write Enable bits (Write protection off)
 
-psect RESET_VECT, class=CODE, delta=2	; same as ORG, indicate the start position of the program
+; starting position of the program
+psect RESET_VECT, class=CODE, delta=2
 RESET_VECT:
-    GOTO setup
-	
-psect INT_VECT, class=CODE, delta=2		; indicate the memory location to go when a interrupt happens
+    GOTO    setup
+
+; memory location to go when a interrupt happens
+psect INT_VECT, class=CODE, delta=2
 INT_VECT:
-	; implement method interruption
-	
+    
+    ; IMPLEMENT METHOD INTERRUPTION
+    
     RETFIE
 
 ; program variables
-W_REG   EQU 0
-F_REG   EQU 1
+W_REG		EQU 0
+F_REG		EQU 1
+#SENSITIVITY_1	EQU 0x20
+#SENSITIVITY_2	EQU 0x21
+STP_STEPS	EQU 0x22    ; number of steps per revolution
+STP_DELAY	EQU 0x23    ; delay between steps
+STP_ACCEL	EQU 0x24    ; initial acceleration
+STP_MAXSPEED	EQU 0x25    ; maximum speed
+STP_MINSPEED	EQU 0x26    ; minimum speed
 
-CTER_0  EQU 0X20
-CTER_1  EQU 0X21
-CTER_2  EQU 0X22
-
+; program setup
 setup:
-	
+    
+    ; configuration of inputs/outputs pins
+    BANKSEL TRISA
+    MOVLW   0b00001111		; configuration of the first 4 pins of port-A as analog input
+    MOVWF   TRISA
+    BANKSEL TRISB
+    MOVLW   0b00000000		; configuration of the first 4 pins of port-B as digital output
+    MOVWF   TRISB
+    
+    ; configuration of ADC
+    BANKSEL ADCON0
+    MOVLW   0b10000001		; turn on the ADC and select channel AN0 as input
+    MOVWF   ADCON0
+    MOVLW   0b00000110		; set the reference voltage in VDD and VSS
+    MOVWF   ADCON1
+    
+    ; output pins configuration for stepper motors
+    BANKSEL PORTB
+    MOVLW   0b00000000		; initialize port-B pins as low output
+    MOVWF   PORTB
 
+    ; configuration of stepper motor parameters
+    MOVWF   STP_STEPS
+    MOVWF   STP_DELAY
+    MOVWF   STP_ACCEL
+    MOVWF   STP_MAXSPEED
+    MOVWF   STP_MINSPEED
+
+; main program loop
 main:
-	
-    GOTO main
+    
+    ; read the value of the analog input AN0
+    BANKSEL ADCON0
+    BSF	    ADCON0, GO		; start conversion
+waitForADC:
+    BTFSC   ADCON0, GO		; wait for conversion to finish
+    GOTO    waitForADC
+    MOVF    ADRESH, W		; read the converted value
+    BANKSEL PORTB
+
+    ; control stepper motors according to light intensity measured by LDR sensors
+    ; stepper motor 1
+    MOVF    ADRESH, W		; read the converted value
+    SUBWF   #SENSITIVITY_1, W	; subtract sensor sensitivity 1
+    BTFSC   STATUS, C		; if the result is negative, sensor 1 is dark
+    CALL    rotateLeft		; rotate the stepper motor to the left
+    BTFSS   STATUS, C		; if the result is positive, sensor 1 is illuminated
+    CALL    rotateRight		; rotate the stepper motor to the right
+    
+    ; stepper motor 2
+    MOVF    ADRESH, W		; read the converted value
+    SUBWF   #SENSITIVITY_2, W	; subtract sensor sensitivity 2
+    BTFSC   STATUS, C		; if the result is negative, sensor 2 is dark
+    CALL    rotateUp		; rotate the stepper motor up
+    BTFSS   STATUS, C		; if the result is positive, sensor 2 is illuminated
+    CALL    rotateDown		; rotate the stepper motor down
+    
+    ; return to main program loop
+    GOTO    main
+    
+rotateLeft:
+    
+    ; IMPLEMENT METHOD
+    
+    RETURN
+    
+rotateRight:
+    
+    ; IMPLEMENT METHOD
+    
+    RETURN
+    
+rotateUp:
+    
+    ; IMPLEMENT METHOD
+    
+    RETURN
+    
+rotateDown:
+    
+    ; IMPLEMENT METHOD
+    
+    RETURN
 
 END RESET_VECT
